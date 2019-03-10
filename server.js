c185a5cf37eb9d203c4f6fb623dc0d6442e92586
Minh Hiếu Phạm <pmh.ars14@gmail.com>
2019-03-10 17:59:51 +0700

    port 80

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 80;
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
